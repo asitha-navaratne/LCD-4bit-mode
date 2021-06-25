5f1e5af6c35d994251a07bc333063c3d699b6440
Asitha Navaratne <asithan476@gmail.com>
2021-06-25 22:15:05 +0530

    Justified comments and code

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 
 #define LCD_DDR 		DDRB									///< DDR of LCD display.
 #define LCD_PORT 		PORTB									///< Port of LCD display.
-#define RS 				PB0										///< Pin for LCD Register Select terminal.
-#define EN 				PB1										///< Pin for LCD Enable terminal.
+#define RS 			PB0									///< Pin for LCD Register Select terminal.
+#define EN 			PB1									///< Pin for LCD Enable terminal.
 
 void LCD_DISPLAY_INIT(void);
 void LCD_SEND_COMMAND(unsigned char command);
@@ -17,10 +17,10 @@ int main(void){
 	LCD_DISPLAY_INIT();
 	
 	LCD_SEND_STRING("Asitha");
-	LCD_SEND_COMMAND(0xC0);										///< Move cursor to the beginning of second line.
+	LCD_SEND_COMMAND(0xC0);							///< Move cursor to the beginning of second line.
 	LCD_SEND_STRING("Navaratne");
 	
-	LCD_SEND_COMMAND(0x0C);										///< Turn cursor off.
+	LCD_SEND_COMMAND(0x0C);							///< Turn cursor off.
 }
 
 /*!
@@ -30,11 +30,11 @@ int main(void){
 void LCD_DISPLAY_INIT(void){
 	LCD_DDR |= (1<<RS)|(1<<EN)|(1<<4)|(1<<5)|(1<<6)|(1<<7);
 	
-	LCD_SEND_COMMAND(0x01);										///< Clear display screen.
-	LCD_SEND_COMMAND(0x02);										///< Initialize LCD in 4-bit mode.
-	LCD_SEND_COMMAND(0x28);										///< 2 line, 5x7 matrix of characters in 4-bit mode.
-	LCD_SEND_COMMAND(0x80);										///< Send cursor to beginning of first line.
-	LCD_SEND_COMMAND(0x0E);										///< Turn display and cursor on.
+	LCD_SEND_COMMAND(0x01);							///< Clear display screen.
+	LCD_SEND_COMMAND(0x02);							///< Initialize LCD in 4-bit mode.
+	LCD_SEND_COMMAND(0x28);							///< 2 line, 5x7 matrix of characters in 4-bit mode.
+	LCD_SEND_COMMAND(0x80);							///< Send cursor to beginning of first line.
+	LCD_SEND_COMMAND(0x0E);							///< Turn display and cursor on.
 	_delay_ms(2);
 }
 
@@ -45,17 +45,17 @@ void LCD_DISPLAY_INIT(void){
 
 void LCD_SEND_COMMAND(unsigned char command){
 	LCD_PORT = (LCD_PORT & 0x0F)|(command & 0xF0);				///< Load upper nibble of the command to the port.
-	LCD_PORT &= ~(1<<RS);										///< Set RS pin low to enter command mode.
+	LCD_PORT &= ~(1<<RS);							///< Set RS pin low to enter command mode.
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Create a momentary pulse at EN pin to send the data to the LCD.
+	LCD_PORT &= ~(1<<EN);							///< Create a momentary pulse at EN pin to send the data to the LCD.
  
 	_delay_us(200);
  
 	LCD_PORT = (LCD_PORT & 0x0F)|(command << 4);				///< Load lower nibble of the command to the port.
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Pulse to send the data.
+	LCD_PORT &= ~(1<<EN);							///< Pulse to send the data.
  
 	_delay_ms(2);
 }
@@ -67,17 +67,17 @@ void LCD_SEND_COMMAND(unsigned char command){
 
 void LCD_SEND_CHARACTER(unsigned char character){
 	LCD_PORT = (LCD_PORT & 0x0F)|(character & 0xF0);			///< Load upper nibble of the character to the port. 
-	LCD_PORT |= (1<<RS);										///< Set RS pin high to enter character mode.
+	LCD_PORT |= (1<<RS);							///< Set RS pin high to enter character mode.
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Create a momentary pulse at EN pin to send the data to the LCD.
+	LCD_PORT &= ~(1<<EN);							///< Create a momentary pulse at EN pin to send the data to the LCD.
 	
 	_delay_us(200);
 
 	LCD_PORT = (LCD_PORT & 0x0F)|(character << 4);				///< Load lower nibble of the character to the port. 
 	LCD_PORT |= (1<<EN);
 	_delay_us(1);
-	LCD_PORT &= ~(1<<EN);										///< Pulse to send the data.
+	LCD_PORT &= ~(1<<EN);							///< Pulse to send the data.
 	
 	_delay_ms(2);
 }
